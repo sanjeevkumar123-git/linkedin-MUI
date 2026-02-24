cdbd8e3a2cee39e0dcebc8a17eafb980dfcddcc2
sanjeevkumar123-git <184698834+sanjeevkumar123-git@users.noreply.github.com>
2026-02-24 10:05:17 +0530

    Refactor Grid item properties for responsiveness

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
+import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 
 import AppTopBar from "../components/AppToBar";
 import LeftSidebar from "../components/LeftSidebar";
@@ -11,17 +11,18 @@ export default function HomePage() {
   return (
     <Box>
       <AppTopBar />
+
       <Container sx={{ mt: 2, mb: 4 }}>
         <Grid container spacing={2}>
-          <Grid item md={3}>
+          <Grid item xs={12} md={3}>
             <LeftSidebar />
           </Grid>
 
-          <Grid item xs={12} md={6} lg={6}>
+          <Grid item xs={12} md={6}>
             <Feed />
           </Grid>
 
-          <Grid item lg={3}>
+          <Grid item xs={12} md={3}>
             <RightSidebar />
           </Grid>
         </Grid>
